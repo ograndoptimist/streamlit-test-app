f855b91c04f2ae68b3ca9ad5aad38eac289bb208
Gabriel <gabrieliprj@gmail.com>
2024-04-01 16:17:54 -0300

    first commit

---
+++ chat.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+
+st.title("ChatGPT-like clone")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = "Hello!"
+        response = st.write(stream)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
